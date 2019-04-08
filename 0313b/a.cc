5ba3aa0fb59efe8df7d170d5bdc790888bb3552b
baqkqpfhv <846512356@qq.com>
2019-04-08 11:53:18 +0800

    none

--- 0313b/a.cc
+++ 0313b/a.cc
@@ -5,54 +5,46 @@
 
 using namespace std;
 
+bool b[1005];
+
+int dfs(const char s[], int L, int R, int k)
+{
+	//cout << L << ' ' << R << ' ' << k << endl;
+	if (k == 0 || L == R)
+		return 0;
+	int p = L;
+	for (int i = L + 1; i < R; i++)
+	{
+		if (s[i] < s[p])
+			p = i;
+	}
+	b[p] = true;
+	int r = dfs(s, p + 1, R, k - 1);
+	int l = r + 1 == k ? 0 : dfs(s, L, p, k - 1 - r);
+	return l + r + 1;
+}
+
 int main()
 {
-	static char _s[1005], *s;
-	static int a[1005], m, p[1005];
-	static bool vis[1005];
-	while (~scanf("%s%d", _s, &m))
+	static char s[1005];
+	int m;
+	while (~scanf("%s%d", s, &m))
 	{
-		s = _s;
-		while (m && s[0])
-		{
-			char *t = s;
-			while (t[0] && t[0] != '0')
-				t++;
-			if (!t[0])
-				break;
-			if (t - s <= m)
-				m -= t - s;
-			else
-				break;
-			s = t;
-			do
-				s++;
-			while (s[0] == '0');
-		}
-		if (!s[0])
-		{
-			puts("0");
-			continue;
-		}
 		int n = strlen(s);
+		m = n - m;
+		memset(b, false, sizeof(b));
+		dfs(s, 0, n, m);
+		bool bb = false;
 		for (int i = 0; i < n; i++)
 		{
-			a[i] = s[i] - '0';
-			p[i] = i;
-		}
-		sort(p, p + n, [](int x, int y) { return a[x] > a[y] || a[x] == a[y] && x < y; });
-		memset(vis, true, sizeof(vis));
-		for (int i = 0; i < m; i++)
-			vis[p[i]] = false;
-		bool b = false;
-		for (int i = 0; i < n; i++)
-		{
-			if (!vis[i] || !b && a[i] == 0)
+			if (!b[i])
 				continue;
-			printf("%d", a[i]);
-			b = true;
+			if (s[i] == '0' && !bb)
+				continue;
+			bb = true;
+			putchar(s[i]);
 		}
-		puts(b ? "" : "0");
+		puts(bb ? "" : "0");
 	}
 	return 0;
 }

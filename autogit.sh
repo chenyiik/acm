4a1c2b702fdbafbe8e101f791b6e96b58f490bc5
baqkqpfhv <ChenYiik@outlook.com>
2019-04-12 14:24:14 +0800

    none

--- autogit.sh
+++ autogit.sh
@@ -1,2 +1,2 @@
 git add . 
-git commit -m 'none' && (git push origin master || git pull origin master && git push origin master)
+git commit -m 'none' && (git push origin master || (git pull origin master && git push origin master))

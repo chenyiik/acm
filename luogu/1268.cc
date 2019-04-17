e4a5a1f16f707d3861bf95fc8a5bc259360a658b
baqkqpfhv <ChenYiik@outlook.com>
2019-04-17 12:18:51 +0800

    update luogu/1268.cc

--- luogu/1268.cc
+++ luogu/1268.cc
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -24,7 +25,7 @@ int main()
 		static vector<int> G[N * 100];
 		static int dist[N * 100][N];
 		static int u[N];
-		int nodes = 0, edges = 0;
+		int nodes = 0;
 		u[1] = 0, u[2] = a[1][2];
 		if (a[1][2] != 0)
 		{
@@ -48,9 +49,41 @@ int main()
 		}
 		for (int i = 3; i <= n; i++)
 		{
-
+			int s = 0, d = 0;
+			for (int j = 0; j < nodes; j++)
+			{
+				d = a[i][1] - dist[j][1];
+				bool b = true;
+				for (int k = 2; k < i; k++)
+				{
+					if (a[i][k] - dist[j][k] != d)
+					{
+						b = false;
+						break;
+					}
+				}
+				if (b)
+				{
+					s = j;
+					break;
+				}
+			}
+		//static vector<int> G[N * 100];
+		//static int dist[N * 100][N];
+		//static int u[N];
+			u[i] = s + d;
+			dist[nodes + d][i] = 0;
+			if (d != 0)
+			{
+				for (int j = 0; j < d; j++)
+				{
+					G[nodes + j].push_back(nodes + j + 1);
+					dist[nodes + j][i] = d - j;
+				}
+				G[nodes + d].push_back(nodes + d - 1);
+			}
 		}
-		printf("%d\n", edges);
+		printf("%d\n", nodes);
 	}
 	return 0;
 }

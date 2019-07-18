52d7bc6cf3adccfa1e65597b5ad16ce4075c8b1c
CHEN Yiik <chenyiik@outlook.com>
2019-07-18 16:16:39 +0800

    none

--- 0718-nc/b.cc
+++ 0718-nc/b.cc
@@ -40,9 +40,19 @@ int main()
 		for (int i = 0; i < n; i++)
 			scanf("%lld", &a[i]);
 		double ans = 0.0;
-		for (int i = 1; i < 10000 * 10000; i++)
-			ans += f(a, n, i / 10000.0);
-		cout << PI / ans * 10000 << endl;
+		for (ll l = 0, r = 1, d = 10000000; r <= 1000000; l = l ? l * 10 : 1, r *= 10, d /= 10)
+		{
+			double tmp = 0.0;
+			for (int i = l * d + 1; i <= r * d; i++)
+				ans += f(a, n, i / (double)d) / d;
+			ans += tmp;
+		}
+		/*
+		const int t1 = 10000, t2 = 10000;
+		for (int i = 1; i <= t1 * t2; i++)
+			ans += f(a, n, i / (double)t2) / t2;
+		*/
+		cout << PI / ans << endl;
 	}
 	return 0;
 }

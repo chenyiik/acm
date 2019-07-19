d9cf0edc6a92965e0db9c8b15a1ba6315567953c
CHEN Yiik <chenyiik@outlook.com>
2019-07-20 02:12:45 +0800

    none

--- 0718-nc/b.cc
+++ 0718-nc/b.cc
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <cstring>
-#include <cmath>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
+
+const int maxn = 1005, mod = int(1e9) + 7;
+ll a[maxn];
 
 ll pow_mod(ll a, ll p, ll n)
 {
@@ -18,42 +20,29 @@ ll pow_mod(ll a, ll p, ll n)
 		ans = ans * a % n;
 	return ans;
 }
-
-ll inv(ll a, ll n) { return pow_mod(a, n - 2, n); }
-const double PI = acos(-1.0);
-
-double f(const ll a[], ll n, double x)
-{
-	double fenmu = 1.0;
-	for (int i = 0; i < n; i++)
-		fenmu *= (a[i] * a[i] + x * x);
-	return 1 / fenmu;
-}
+ll inv(ll a, ll n = mod) { return pow_mod(a, n - 2, n); }
 
 int main()
 {
-	const ll mod = int(1e9) + 7;
-	ll n;
-	static ll a[1005];
-	while (~scanf("%lld", &n))
+	int n;
+	while (~scanf("%d", &n))
 	{
 		for (int i = 0; i < n; i++)
 			scanf("%lld", &a[i]);
-		double ans = 0.0;
-		for (ll l = 0, r = 1, d = 10000000; r <= 1000000; l = l ? l * 10 : 1, r *= 10, d /= 10)
+		ll ans = 0;
+		for (int i = 0; i < n; i++)
 		{
-			double tmp = 0.0;
-			for (int i = l * d + 1; i <= r * d; i++)
-				ans += f(a, n, i / (double)d) / d;
-			ans += tmp;
+			ll tmp = 2 * a[i] % mod;
+			for (int j = 0; j < n; j++) if (j != i)
+			{
+				ll ump = (a[j] * a[j] - a[i] * a[i]) % mod;
+				if (ump < 0)
+					ump += mod;
+				tmp = tmp * ump % mod;
+			}
+			ans = (ans + inv(tmp)) % mod;
 		}
-		printf("%.6f\n", PI / ans);
-		/**
-		const int t1 = 10000, t2 = 10000;
-		for (int i = 1; i <= t1 * t2; i++)
-			ans += f(a, n, i / (double)t2) / t2;
-		*/
-		//cout << PI / ans << endl;
+		printf("%lld\n", ans);
 	}
 	return 0;
 }

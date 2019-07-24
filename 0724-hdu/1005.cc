cb3e32294a49d04bf9a82cdcc8afbffa53eb7fee
CHEN Yiik <chenyiik@outlook.com>
2019-07-24 16:07:56 +0800

    none

--- 0724-hdu/1005.cc
+++ 0724-hdu/1005.cc
@@ -22,16 +22,36 @@ LL pow_mod(LL a, LL p, LL n = mod)
 
 LL inv(LL a, LL n = mod) { return pow_mod(a, n - 2, n); }
 
-const int maxn = 3000;
-LL dd[maxn + 1];
+const int maxn = 3000 + 1;
+LL a[maxn], c[maxn][maxn], ans[maxn], tot[maxn];
+
+void init()
+{
+	LL iv4 = inv(4);
+	for (LL i = 1; i < maxn; i++)
+		a[i] = i * (i - 1) % mod * iv4 % mod;
+	for (int i = 0; i < maxn; i++)
+	{
+		c[i][0] = 1;
+		for (int j = 1; j <= i; j++)
+			c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % mod;
+	}
+	for (int k = 1; k < maxn; k++)
+	{
+		LL &kk = ans[k], ivk = inv((pow_mod(2, k) + mod - 1) % mod);
+		kk += a[k];
+		for (int i = 0, j = k; j > 1; i++, j--)
+			kk = (kk + c[k][i] * a[j] % mod * ivk) % mod;
+	}
+	for (int i = 1; i < maxn; i++)
+		tot[i] = (tot[i - 1] + ans[i]) % mod;
+}
 
 int main()
 {
+	init();
 	LL n;
 	while (~scanf("%lld", &n))
-	{
-		LL ans = 2 * pow_mod(n - 1, 2) * inv(3) % mod * inv(n) % mod;
-		printf("%lld\n", ans);
-	}
+		printf("%lld\n", tot[n] * inv(n) % mod);
 	return 0;
 }

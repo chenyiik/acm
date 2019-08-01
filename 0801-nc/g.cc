bf2172dd4b8a3eacd416a8c71511d7c769ccded5
CHEN Yiik <chenyiik@outlook.com>
2019-08-01 15:39:50 +0800

    none

--- 0801-nc/g.cc
+++ 0801-nc/g.cc
@@ -20,17 +20,15 @@ void init()
 	}
 }
 
-bool cmp(char a, char b) { return a >= b; }
-bool cmq(char a, char b) { return a == b; }
-
-long long f(int i, int j, bool c(char, char))
+long long f(int i, int j)
 {
-	if (j == m) return 1;
-	if (i == n) return 0;
+	if (i == n || j == m) return 0;
 	if (dp[i][j] > -1) return dp[i][j];
-	if (c(s[i], t[j])) 
-		return dp[i][j] = (f(i + 1, j, c) + f(i + 1, j + 1, c)) % mod;
-	return dp[i][j] = f(i + 1, j, c);
+	if (s[i] > t[j])
+		return dp[i][j] = (f(i + 1, j) + c[n - i - 1][m - j - 1]) % mod;
+	if (s[i] == t[j]) 
+		return dp[i][j] = (f(i + 1, j) + f(i + 1, j + 1)) % mod;
+	return dp[i][j] = f(i + 1, j);
 }
 
 int main()
@@ -48,10 +46,11 @@ int main()
 				for (int j = m; j <= n - i - 1; j++)
 					ans = (ans + c[n - i - 1][j]) % mod;
 			}
-		memset(dp, -1, sizeof(dp));
-		ans = (ans + f(0, 0, cmp)) % mod;
-		memset(dp, -1, sizeof(dp));
-		ans = (ans - f(0, 0, cmq) + mod) % mod;
+		//memset(dp, -1, sizeof(dp));
+		for (int i = 0; i < n + 5; i++)
+			for (int j = 0; j < m + 5; j++)
+				dp[i][j] = -1;
+		ans = (ans + f(0, 0)) % mod;
 		printf("%lld\n", ans);
 	}
 	return 0;

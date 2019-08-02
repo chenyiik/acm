254e3285559f472fb5b4246bca5e997281f09778
CHEN Yiik <chenyiik@outlook.com>
2019-08-02 15:00:19 +0800

    none

--- 0724-hdu/1011.cpp
+++ 0724-hdu/1011.cpp
@@ -1,94 +1,93 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int fibt=50;
-vector<long long> hive[int(4e5+50)];
-int used=0;
-int alloc()
+const size_t maxN=1e5+5;
+namespace divided_tree
 {
-	hive[used].clear();
-	hive[used].reserve(fibt+1);
-	return used++;
-}
-struct node
-{
-	int v;
-	int l,r;
-}tree[int(4e5+50)];
-long long a[int(1e5+5)];
-vector<long long> merge2(const vector<long long>& a,const vector<long long>& b)
-{
-	vector<long long> c;
-	int i=0,j=0;
-	while (i<a.size() && j<b.size() && c.size()<fibt)
-	if (a[i]>=b[j]) c.push_back(a[i++]);
-	else c.push_back(b[j++]);
-	while (i<a.size() && c.size()<fibt) c.push_back(a[i++]);
-	while (j<b.size() && c.size()<fibt) c.push_back(b[j++]);
-	return c;
-}
-vector<long long> merge(const vector<long long>& a,const vector<long long>& b)
-{
-	vector<long long> c(a.size()+b.size());
-	merge(a.begin(),a.end(),b.begin(),b.end(),c.begin(),[](const long long &x,const long long &y){return x>y;});
-	if (c.size()>fibt) c.resize(fibt);
-	return c;
-}
-void up(int x)
-{
-	hive[tree[x].v]=std::move(merge(hive[tree[x<<1].v],hive[tree[x<<1|1].v]));
-}
-void build(int x,int l,int r)
-{
-	tree[x].v=alloc();
-	tree[x].l=l,tree[x].r=r;
-	if (l==r) 
+	long long tree[25][maxN];
+	long long sorted[maxN];
+	long long toLeft[25][maxN];
+	void init()
 	{
-		hive[tree[x].v].push_back(a[l]);
-		/*cout<<tree[x].l<<" "<<tree[x].r<<" ";
-		for (auto xx:tree[x].v) cout<<xx<<" ";
-		cout<<endl;*/
-		return;
-	}
-	int mid=(l+r)>>1;
-	build(x<<1,l,mid);
-	build(x<<1|1,mid+1,r);
-	up(x);
-	/*cout<<tree[x].l<<" "<<tree[x].r<<" ";
-	for (auto xx:tree[x].v) cout<<xx<<" ";
-	cout<<endl;*/
-}
-vector<long long> query(int x,int l,int r)
-{
-	vector<long long> ans;
-	int ll=tree[x].l,rr=tree[x].r;
-	if (ll>=l && rr<=r) return hive[tree[x].v];
-	int mid=(ll+rr)>>1;
-	if (l<=mid) ans=merge(ans,query(x<<1,l,r));
-	if (r>mid) ans=merge(ans,query(x<<1|1,l,r));
-	return ans;
-}
-int main()
-{
-	int n,q;
-	ios::sync_with_stdio(0);cin.tie(0);
-	while(cin>>n>>q)
+		memset(tree,0,sizeof(tree));
+    	memset(toLeft,0,sizeof(toLeft));
+    }
+	void build(long long l,long long r,long long dep) 
 	{
-		used=0;
-		for (int i=1;i<=n;i++) cin>>a[i];
-		build(1,1,n);
-		while (q--)
+	    if (l==r) return;
+	    long long mid=(l+r)>>1;
+	    long long same=mid-l+1;
+	    long long smid=sorted[mid];
+		for (long long i=l;i<=r;i++)
+	    if (tree[dep][i]<smid) --same;
+	    long long lpos=l,rpos=mid+1;
+		for (long long i=l;i<=r;i++)
 		{
-			int l,r;
-			cin>>l>>r;
-			long long ans=-1;
-			vector<long long> res=std::move(query(1,l,r));
-			for (int i=0;i<res.size()-2;i++)
+	        if (tree[dep][i] < smid) tree[dep + 1][lpos++] = tree[dep][i];
+			else if (tree[dep][i]==smid && same>0) 
 			{
-				long long x=res[i],y=res[i+1],z=res[i+2];
-				if (x<y+z) {ans=x+y+z;break;}
-			}
-			cout<<ans<<endl;
-		}
+	            --same;
+	            tree[dep+1][lpos++]=tree[dep][i];
+	        } 
+			else tree[dep+1][rpos++]=tree[dep][i];
+	        toLeft[dep][i]=toLeft[dep][l-1]+lpos-l;
+	    }
+	    build(l,mid,dep+1);
+	    build(mid+1,r,dep+1);
 	}
-	return 0;
+	
+	long long get_kth_small(long long L,long long R,long long l,long long r,long long dep,long long k) 
+	{
+	    if (l==r) return tree[dep][l];
+	    long long mid=(L+R)>>1;
+	    long long cnt=toLeft[dep][r]-toLeft[dep][l-1];
+	    if (cnt>=k) 
+		{
+	        long long newl=L+toLeft[dep][l-1]-toLeft[dep][L-1];
+	        long long newr=newl+cnt-1;
+	        return get_kth_small(L,mid,newl,newr,dep+1,k);
+	    } 
+		else 
+		{
+	        long long newr=r+toLeft[dep][R]-toLeft[dep][r];
+	        long long newl=newr-(r-l-cnt);
+	        return get_kth_small(mid+1,R,newl,newr,dep+1,k-cnt);
+	    }
+	}
+};
+int main() 
+{
+	long long N,Q;
+	while (~scanf("%lld%lld",&N,&Q))
+	{
+		divided_tree::init();
+	    for (int i=1;i<=N;i++) 
+		{
+	        scanf("%lld",&divided_tree::tree[0][i]);
+	        divided_tree::sorted[i]=divided_tree::tree[0][i];
+	    }
+	    sort(divided_tree::sorted+1,divided_tree::sorted+N+1);
+	    divided_tree::build(1,N,0);
+	    while (Q--)
+		{
+	        long long s,t;
+			scanf("%lld%lld",&s,&t);
+	        if (t-s+1<3) {puts("-1");continue;}
+	        long long ans=-1;
+	        long long l=t-s+1;
+	        long long cnt=0;
+	        long long l1=divided_tree::get_kth_small(1,N,s,t,0,l),l2=divided_tree::get_kth_small(1,N,s,t,0,l-1);
+	        for (long long k=l-2;k>=1 && cnt<=50;k--,cnt++)
+			{
+				 long long l3=divided_tree::get_kth_small(1,N,s,t,0,k);
+				 if (l1<l2+l3)
+				 {
+				 	ans=l1+l2+l3;
+				 	break;
+				 }
+				 else {l1=l2;l2=l3;}
+			}
+	        printf("%lld\n",ans);
+	    }
+	}
+    return 0;
 }

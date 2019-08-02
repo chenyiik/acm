dfb0ac1dec5c1a04e7ebfd605d6cf77c60062225
CHEN Yiik <chenyiik@outlook.com>
2019-08-02 15:47:35 +0800

    none

--- 0724-hdu/1005.cc
+++ 0724-hdu/1005.cc
@@ -23,28 +23,29 @@ LL pow_mod(LL a, LL p, LL n = mod)
 LL inv(LL a, LL n = mod) { return pow_mod(a, n - 2, n); }
 
 const int maxn = 3000 + 1;
-LL a[maxn], c[maxn][maxn], ans[maxn], tot[maxn];
+LL c[maxn][maxn], ans[maxn], tot[maxn];
+
+LL f(LL i)
+{
+	if (ans[i] > -1) return ans[i];
+	LL p = 0;
+	for (int j = 0; j < i; j++)
+		p = (p + c[i][j] * f(j)) % mod;
+	LL q = inv((pow_mod(2, i) - 1 + mod) % mod);
+	return ans[i] = p * q % mod;
+}
 
 void init()
 {
-	LL iv4 = inv(4);
-	for (LL i = 1; i < maxn; i++)
-		a[i] = i * (i - 1) * iv4 % mod;
 	for (int i = 0; i < maxn; i++)
 	{
 		c[i][0] = 1;
 		for (int j = 1; j <= i; j++)
 			c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % mod;
 	}
-	for (int k = 1; k < maxn; k++)
-	{
-		LL &kk = ans[k];
-		for (int i = 0; i < k; i++)
-			kk = (kk + c[k][i] * a[k - i] % mod) % mod;
-		kk = kk * inv((pow_mod(2, k) + mod - 1) % mod) % mod;
-		kk = (kk + a[k]) % mod;
-	}
-	for (int i = 1; i < maxn; i++)
+	memset(ans, -1, sizeof(ans));
+	ans[0] = 0, ans[1] = 1, f(3000);
+	for (int i = 1; i <= 3000; i++)
 		tot[i] = (tot[i - 1] + ans[i]) % mod;
 }
 
@@ -52,6 +53,7 @@ int main()
 {
 	init();
 	LL n;
+	cout << inv(6) << endl;
 	while (~scanf("%lld", &n))
 		printf("%lld\n", tot[n] * inv(n) % mod);
 	return 0;

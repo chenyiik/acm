7d5902592836f81aa67b0ffd30916062a5d7f0d6
CHEN Yiik <chenyiik@outlook.com>
2019-08-04 23:58:54 +0800

    none

--- butogit.sh
+++ butogit.sh
@@ -1,8 +1,4 @@
 git add . 
-if [ "$1" ]; then
-	git commit -m "$1"
-else
-	git commit -m 'none'
-fi
+git commit -m 'none'
 git push origin2 master || (git pull origin2 master && git push origin2 master)
 

2c6a3134da248ac163ea1d55066aaf7718ca64f1
Ctumille <wz415662910@gmail.com>
2019-08-11 15:46:23 +0800

    none

---
+++ get_Nowcoder_YTL/get_Nowcoder_YTL.py
@@ -0,0 +1,35 @@
+import re
+import requests
+import time
+import csv
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+try:
+	options = webdriver.FirefoxOptions()
+	options.add_argument("--headless")
+	driver = webdriver.Firefox(options=options)
+except:
+	options = webdriver.ChromeOptions()
+	options.add_argument("--headless")
+	driver = webdriver.Chrome(options=options)
+try:
+	for contest_id in range(881,891):
+		url="https://ac.nowcoder.com/acm/contest/{}#question".format(contest_id)
+		driver.get(url)
+		wait = WebDriverWait(driver, 10)
+		table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'table-hover')))
+		table_rows = table.find_elements_by_tag_name("tr")
+		print(url)
+		for i in range(1,len(table_rows)):
+			table_columns = table_rows[i].find_elements_by_tag_name("td")
+			str=table_columns[0].text+" "+table_columns[4].text.split('/')[0]
+			print(str)
+		print("")
+except:
+	driver.close()
+else:
+	driver.close()

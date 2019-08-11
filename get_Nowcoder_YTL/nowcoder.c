be18eb0bf8792786bcb40d293f4dcdcb12624ddd
CHEN Yiik <chenyiik@outlook.com>
2019-08-11 16:36:50 +0800

    none

---
+++ get_Nowcoder_YTL/nowcoder.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	//freopen("nowcoder.txt", "r", stdin);
+	//freopen("nowcoder.md", "w", stdout);
+	puts("## nowcoder");
+	static const int date[10] = {718, 720, 725, 727, 801, 803, 808, 810, 815, 817}, off = 881;
+	const char maxch = 'K';
+	printf("| |");
+	for (char c = 'A'; c <= maxch; c++)
+		printf(" %c |", c);
+	puts("");
+	printf("| :-: |");
+	for (char c = 'A'; c <= maxch; c++)
+		printf(" :-: |");
+	puts("");
+	static char st[100];
+	scanf("%s", st);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("| [0%d](%s) |", date[i], st);
+		char c = 'A';
+		int b;
+		while (b = ~scanf("%s", st))
+		{
+			if (strlen(st) > 1)
+				break;
+			scanf("%s", st);
+			printf(" %s |", st);
+			c++;
+		}
+		while (c++ <= maxch)
+			printf(" - |");
+		puts("");
+		if (b == 0)
+			break;
+	}
+}
---
+++ get_Nowcoder_YTL/nowcoder.sh
@@ -0,0 +1,2 @@
+gcc nowcoder.c -o nowcoder.exe
+python3 get_Nowcoder_YTL.py | ./nowcoder.exe > nowcoder.md

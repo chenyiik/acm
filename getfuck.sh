ea1ea8fd2cc8a827de4a1f87838b13c742b36629
CHEN Yiik <chenyiik@outlook.com>
2019-08-13 16:36:32 +0800

    a

--- getfuck.sh
+++ getfuck.sh
@@ -1,7 +1,7 @@
-cd get_Nowcoder_YTL
-./nowcoder.sh > ../fuck.md.swp
-cd ../get_HDU_YTL
-./hdu.sh >> ../fuck.md.swp
-cd ..
-rm fuck.md
+cd get_Nowcoder_YTL && 
+./nowcoder.sh > ../fuck.md.swp &&
+cd ../get_HDU_YTL &&
+./hdu.sh >> ../fuck.md.swp && 
+cd .. && 
+rm fuck.md &&
 mv fuck.md.swp fuck.md

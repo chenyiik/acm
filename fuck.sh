8caa083fa56d38301e1f097f5b19ec3ac127648e
CHEN Yiik <chenyiik@outlook.com>
2019-08-14 00:43:11 +0800

    none

--- fuck.sh
+++ fuck.sh
@@ -4,5 +4,4 @@ python3 get_Nowcoder_YTL.py | ./nowcoder.exe > unsolved.md.swp &&
 echo -e 'Nowcoder fucking complete!\n\nFucking HDU star...' &&
 python3 get_HDU_YTL.py | ./hdu.exe >> unsolved.md.swp &&
 echo 'HDU fucking complete!' &&
-rm ../unsolved.md 
-mv unsolved.md.swp ../unsolved.md
+(rm ../unsolved.md; mv unsolved.md.swp ../unsolved.md)

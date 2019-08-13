6b1fc087d598d6becef5f02d2a12dbb3157126de
Ctumille <wz415662910@gmail.com>
2019-08-13 17:44:29 +0800

    none

--- get_Nowcoder_YTL/get_Nowcoder_YTL.py
+++ get_Nowcoder_YTL/get_Nowcoder_YTL.py
@@ -1,35 +1,19 @@
 import time
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import json
+import requests
 def main():
-	try:
-		driver = webdriver.Safari()
-	except:
-		options = webdriver.ChromeOptions()
-		options.add_argument("--headless")
-		driver = webdriver.Chrome(options=options)
-	try:
-		for contest_id in range(881,891):
-			url="https://ac.nowcoder.com/acm/contest/{}#question".format(contest_id)
-			driver.get(url)
-			if driver.current_url!=url:
-				break
-			wait = WebDriverWait(driver, 60)
-			table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'table-hover')))
-			table_rows = table.find_elements_by_tag_name("tr")
-			print(url)
-			for i in range(1,len(table_rows)):
-				table_columns = table_rows[i].find_elements_by_tag_name("td")
-				str=table_columns[0].text+" "+table_columns[4].text.split('/')[0]
-				print(str)
-			print("")
-	except:
-		pass
-	finally:
-		driver.close()
+	for contest_id in range(881,891):
+		hyper="https://ac.nowcoder.com/acm/contest/{}#question".format(contest_id)
+		url="https://ac.nowcoder.com/acm/contest/problem-list?token=&id={}".format(contest_id)
+		r=requests.get(url)
+		text=json.loads(r.text)
+		if text["code"]!=0:
+			break
+		print(hyper)
+		for problem in text["data"]["data"]:
+			print("{} {}".format(problem["index"],problem["acceptedCount"]))
+		print("")
 	print("EOF")
 if __name__=='__main__':
 	main()

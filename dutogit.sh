adc5d078d7609919bf7e39217cbe5b1225dfb157
CHEN Yiik <chenyiik@outlook.com>
2019-09-02 17:20:10 +0800

    no response

--- dutogit.sh
+++ dutogit.sh
@@ -6,7 +6,7 @@ com=$(git remote -v | grep '251y3g3220\.qicp\.vip.*push' | awk '{print $1}')
 github=$(git remote -v | grep 'github\.com.*push' | awk '{print $1}')
 
 ping 110.64.92.214 -c 1
-if [ $? == 0]; then
+if [ $? == 0 ]; then
 	git pull $edu master
 	git push $edu master
 else

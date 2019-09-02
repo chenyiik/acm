ad3390351ea9b8fc9dda77b2b81c9d6bdb1ebf5e
CHEN Yiik <chenyiik@outlook.com>
2019-09-02 19:02:39 +0800

    no response

--- fuck.sh
+++ fuck.sh
@@ -2,4 +2,4 @@ cd get && make all &&
 (python3 get_Nowcoder_YTL.py; python3 get_HDU_YTL.py) | 
 ./get.exe > unsolved.md.swp && 
 (rm ../unsolved.md.bak; mv ../unsolved.md ../unsolved.md.bak; mv unsolved.md.swp ../unsolved.md)
-#aaa
+#bbb

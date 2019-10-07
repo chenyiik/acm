a301e1cd8e1412969e2bc9f0027684151b6323eb
CHEN Yiik <chenyiik@outlook.com>
2019-10-07 17:52:33 +0800

    no response

--- 0914-icpc-shenyang/D.cc
+++ 0914-icpc-shenyang/D.cc
@@ -6,30 +6,72 @@
 
 using namespace std;
 
-const int mod = int(1e9) + 7, maxn = int(1e4) + 7;
+typedef long long LL;
+const LL mod = LL(1e9) + 7;
+void add(int &a, LL b) { a = (a + b) % mod; }
+void sub(int &a, LL b) { a = (a - b + mod * mod) % mod; }
 
+const int maxn = int(1e4) + 7;
 struct P { int v, d; };
-
-int ans0, ans1, ans2;
-
 vector<P> G[maxn];
-void solve(int n)
+int sz[maxn][3], val[maxn][3], ans[3];
+
+void dfs(int u, int fa)
 {
-	for (int i = 0; i < n; i++)
-		G[i].clear();
-	for (int i = 1; i < n; i++)
+	sz[u][0]++;
+	for (P q : G[u]) if (q.v != fa)
 	{
-		int u, v, d;
-		scanf("%d%d%d", &u, &v, &d);
-		G[u].push_back({v, d});
-		G[v].push_back({u, d});
+		dfs(q.v, u);
+		for (int i = 0; i < 3; i++)
+		{
+			int j = (q.d + i) % 3;
+			LL t = sz[q.v][i];
+			add(val[u][j], val[q.v][i] + q.d * t);
+			sz[u][j] += t;
+		}
 	}
-	ans0 = ans1 = ans2 = 0;
+}
+
+void dfs(int u, int fa, const int cur[], const int sur[])
+{
+	for (int i = 0; i < 3; i++) add(ans[i], cur[i]);
+	for (P q : G[u]) if (q.v != fa)
+	{
+		int hlf[3], hlg[3]; memcpy(hlf, cur, sizeof(hlf));
+		for (int i = 0; i < 3; i++)
+		{
+			int j = (q.d + i) % 3;
+			LL t = sz[q.v][i];
+			sub(hlf[j], val[q.v][i] + q.d * t);
+			hlg[j] = sur[j] - t;
+		}
+		int nxt[3]; memcpy(nxt, val[q.v], sizeof(nxt));
+		int nxs[3]; memcpy(nxs, sz[q.v], sizeof(nxs));
+		for (int i = 0; i < 3; i++)
+		{
+			int j = (q.d + i) % 3;
+			add(nxt[j], hlf[i] + q.d * LL(hlg[i]));
+			add(nxs[j], hlg[i]);
+		}
+		dfs(q.v, u, nxt, nxs);
+	}
 }
 
 int main()
 {
 	for (int n; ~scanf("%d", &n); )
-		solve(n);
+	{
+		for (int i = 0; i < n; i++) G[i].clear();
+		for (int i = 1; i < n; i++)
+		{
+			int u, v, d; scanf("%d%d%d", &u, &v, &d);
+			G[u].push_back({v, d}), G[v].push_back({u, d});
+		}
+		memset(sz, 0, sizeof(sz[0]) * n);
+		memset(val, 0, sizeof(val[0]) * n);
+		memset(ans, 0, sizeof(ans));
+		dfs(0, -1), dfs(0, -1, val[0], sz[0]);
+		printf("%d %d %d\n", ans[0], ans[1], ans[2]);
+	}
 	return 0;
 }

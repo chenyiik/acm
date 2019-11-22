f1cd1fd6b3424a070ad7b95444434e044e3cec62
CHEN Yiik <chenyiik@outlook.com>
2019-11-22 20:54:41 +0800

    no response

--- noip2019/a.c
+++ noip2019/a.c
@@ -8,13 +8,7 @@ int main()
 	int n; ULL k; scanf("%d%llu", &n, &k);
 	ULL d = 1ULL << (n - 1);
 	for (char *p = s; n--; ++p)
-	{
-		if (k < d)
-			*p = '0';
-		else
-			*p = '1', k = (d << 1) + ~k;
-		d >>= 1;
-	}
+		*p = k < d ? '0' : (k = (d << 1) + ~k, '1'), d >>= 1;
 	puts(s);
 	return 0;
 }

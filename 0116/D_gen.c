0ec3bce93d9b6390368f3feccee3fc3b054adb23
CHEN Yiik <chenyiik@outlook.com>
2020-01-16 23:50:19 +0800

    update about D

--- 0116/D_gen.c
+++ 0116/D_gen.c
@@ -5,14 +5,14 @@
 int main()
 {
 	srand(time(NULL));
-	int n = rand() % 7 + 1, k = rand() % n;
+	int n = rand() % 10 + 1, k = rand() % n;
 	printf("%d %d\n", n, k);
 	while (n--)
 	{
-		int m = rand() % 7;
+		int m = rand() % 11;
 		printf("%d ", m);
 		while (m--)
-			printf("%d ", rand() % 11);
+			printf("%d ", rand() % 30);
 		puts("");
 	}
 	return 0;

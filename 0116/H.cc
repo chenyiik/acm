faed7045ca78d51f91dd4b16c7423b6997983a4d
CHEN Yiik <chenyiik@outlook.com>
2020-01-16 13:44:02 +0800

    0116 HI

---
+++ 0116/H.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int t; scanf("%d", &t);
+	while (t--)
+		puts("1");
+	return 0;
+}

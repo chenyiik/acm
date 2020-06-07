d2a93b4785047d6c39238ce18b5be181d5f031bd
chenyiik <chenyiik@outlook.com>
2020-06-07 13:26:54 +0800

    no response

--- 0607/J.cc
+++ 0607/J.cc
@@ -21,13 +21,14 @@ int main()
 {
 	int a, b, c, d; scanf("%d%d%d%d", &a, &b, &c, &d);
 	int zero = f(a), one = f(d);
-	if ((zero++) == -1 || (one++) == -1 || b + c != zero * one)
-		return puts("impossible"), 0;
+	if (zero++ == -1 || one++ == -1) return puts("impossible"), 0;
+	if (zero * one == b + c) ;
+	else if (zero == 1 && (zero - 1) * one == b + c) zero--;
+	else if (one == 1 && zero * (one - 1) == b + c) one--;
+	else return puts("impossible"), 0;
 	while (zero && one)
-		if (c >= zero)
-			*(p++) = '1', c -= zero, one--;
-		else
-			*(p++) = '0', zero--;
+		if (c >= zero) *(p++) = '1', one--, c -= zero;
+		else *(p++) = '0', zero--;
 	while (zero--) *(p++) = '0';
 	while (one--) *(p++) = '1';
 	puts(s);

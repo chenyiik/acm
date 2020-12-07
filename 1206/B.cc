2e8f5fcd58f410501254e4565dab3c90ffc387e9
chenyiik <chenyiik@outlook.com>
2020-12-07 14:23:49 +0800

    1206B

--- 1206/B.cc
+++ 1206/B.cc
@@ -7,35 +7,27 @@
 using namespace std;
 
 const int maxn = int(1e6) + 10;
-int a[maxn];
+int a[maxn], f[maxn];
 
 int main()
 {
 	int T; scanf("%d", &T);
 	while (T--)
 	{
+		memset(f, 0, sizeof(f));
 		int n; scanf("%d", &n);
 		for (int i = 0; i < n; i++)
+		{
 			scanf("%d", &a[i]);
-		sort(a, a + n);
-		map<int, int> M;
+			f[a[i]]++;
+		}
+		for (int j = 0; j < 20; j++)
+			for (int i = 1; i <= int(1e6); i++)
+				if (i & (1 << j))
+					f[i] += f[i ^ (1 << j)];
 		long long ans = 0;
 		for (int i = 0; i < n; i++)
-		{
-			int &x = a[i];
-			if (M.find(x) == M.end()) M[x] = 1;
-			else M[x]++;
-			ans += M[x] * 2 - 1;
-			while (x)
-			{
-				int bit = 0;
-				for (int j = 0; j < 30; j++)
-					if ((1 << j) & x)
-						bit = j;
-				x &= (1 << bit) - 1;
-				if (M.find(x) != M.end()) ans += M[x];
-			}
-		}
+			ans += f[a[i]];
 		printf("%lld\n", ans);
 	}
 	return 0;

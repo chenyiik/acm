2727250febe27b35ebcfd08078c4cbdd38da0cb5
chenyiik <chenyiik@outlook.com>
2021-04-13 15:02:55 +0800

    0410

---
+++ 0410/A.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int T_T; scanf("%d", &T_T);
+	while (T_T--)
+	{
+		int n; scanf("%d", &n);
+		puts(n ? "You are the future of Tencent!" : "Good luck and Enjoy TPC!");
+	}
+	return 0;
+}

de924573d7df0e6710d043483cb513eae9d0087f
bearx <815018345@qq.com>
2021-05-05 20:52:46 +0800

    no response

---
+++ 0505/F.py
@@ -0,0 +1,17 @@
+n=int(input())
+x=list(map(int,input().split()))
+x=list(zip(x,[i for i in range(len(x))]))
+for i in range(len(x)):
+    x[i]=list(x[i])
+round=0
+while round<1e18:
+    round+=1
+    i,wf,of=0,len(x),0
+    while i<wf:
+        x[i][0]-=(i+1)
+        if x[i][0]<=0:
+            print(x[i][1]+1,end=" ")
+            x=x[:i]+x[i+1:]
+            wf-=1
+        else:
+            i+=1
---
+++ 0505/R.py
@@ -0,0 +1,5 @@
+N=5
+import random
+print(N)
+for i in range(N):
+    print(random.randint(1,10**18),end=" ")
